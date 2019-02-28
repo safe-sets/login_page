498b7506aff03683f9e0ace6d49be6f1d70ad1db
Albert <itzalbert2@gmail.com>
2019-02-27 22:26:03 -0800

    Import helper functions. Need to fix prettier linting

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const crypto = require('crypto');
 const app = express();
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
 const PORT = 9001;
+const {
+  generateSalt,
+  generateHash,
+  saltHash
+} = require('../db/utilities/dbUtilities.js');
 
 app
   .use('/', express.static('public'))
@@ -13,11 +17,14 @@ app
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-const tempPassword = 'password';
-
-const hash = crypto
-  .createHmac('sha256', tempPassword)
-  .update('I love cupcakes')
-  .digest('hex');
+app.get('/login', (req, res) => {
+  const { password } = req.body;
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+//Take user pw
+//Generate a salt
+//Combine salt w/ user entered pw
+//Hash the combined string w/ SHA 256
+//Store result w/ salt in db.
