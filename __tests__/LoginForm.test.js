c691ecb9fd6c6cf35cec832e6373346875244e6a
Albert <itzalbert2@gmail.com>
2019-02-27 20:31:31 -0800

    Adding server file

---
+++ __tests__/LoginForm.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import LoginForm from '../client/src/components/LoginForm.jsx';
+
+describe('LoginForm', () => {
+  it('Should render on the page', () => {
+
+  });
+
+  it('Should require alphanumeric passwords', () => {
+
+  });
+
+  it('Should validate passwords')
+
+  it('Should allow user\'s to register, () => {
+    
+  });
+
+  it('Should validate ')
+
+})
