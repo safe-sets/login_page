84eef547ef9b99cef3c721d916e4bfa1c14862f2
Albert <itzalbert2@gmail.com>
2019-02-28 19:56:01 -0800

    Blueprint for hashed password to send to DB

--- server/server.js
+++ server/server.js
@@ -19,12 +19,11 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/login', (req, res) => {
   const { password } = req.body;
+  const saltLength = generateSalt(16);
+  const hashedPassword = generateHash(password, saltLength);
+  const saltedHash = saltHash(hashedPassword.passwordHash);
+
+  //send saltedHash to the database
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-//Take user pw
-//Generate a salt
-//Combine salt w/ user entered pw
-//Hash the combined string w/ SHA 256
-//Store result w/ salt in db.
