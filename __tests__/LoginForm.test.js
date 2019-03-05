f6cc2de0492bb4a1b1094adfe7148fd0210941c7
Your Name <you@example.com>
2019-03-04 21:46:15 -0800

    Filled out a few tests

--- __tests__/LoginForm.test.js
+++ __tests__/LoginForm.test.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import LoginForm from '../client/src/components/LoginForm.jsx';
+import { shallow } from 'enzyme';
+import { generateSalt, generateHash } from '../db/utilities/dbUtilities.js';
 
 describe('LoginForm', () => {
   it('Should render on the page', () => {
-
+    const wrapper = shallow(<LoginForm />);
+    expect(wrapper).toContain();
   });
 
   it('Should require alphanumeric passwords', () => {
-
+    //Tests for alphanumeric characters, omitting special chars
+    const regExp = /^[a-zA-Z0-9]*$/;
+    expect(regExp.test('string')).toBe(true);
+    expect(regExp.test('onomatopoeia')).toBe(true);
+    expect(regExp.test('ABC123!')).toBe(false);
+    expect(regExp.test('L33THAX0R')).toBe(true);
+    expect(regExp.test('L33TH4X0R!!11!')).toBe(false);
   });
 
-  it('Should validate passwords')
+  it("Should allow user's to register", () => {});
 
-  it('Should allow user\'s to register, () => {
-    
+  it("Should hash the user's password", () => {
+    const passwords = ['sunshine', 'abc123', 'password', 'admin'];
+    //Salts for each of the above passwords
   });
-
-  it('Should validate ')
-
-})
+});
