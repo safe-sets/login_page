d28a0a78ed35c73997c29757d744879fefbe958f
Your Name <you@example.com>
2019-03-04 21:46:01 -0800

    Added comments to server file

--- server/server.js
+++ server/server.js
@@ -2,28 +2,54 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
-const PORT = 9001;
-const {
-  generateSalt,
-  generateHash,
-  saltHash
-} = require('../db/utilities/dbUtilities.js');
+const cors = require('cors');
+const Users = require('../db/models/Users.js');
 
 app
   .use('/', express.static('public'))
   .use(bodyparser.json())
   .use(bodyparser.urlencoded({ extended: true }))
-  .use(morgan('tiny'));
+  .use(morgan('tiny'))
+  .use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/login', (req, res) => {
-  const { password } = req.body;
-  const saltLength = generateSalt(16);
-  const hashedPassword = generateHash(password, saltLength);
-  const saltedHash = saltHash(hashedPassword.passwordHash);
+// If user is already registered, fetch their info from db for validation
+app.get('/userLogin', (req, res) => {
+  const { userName } = req.body;
 
-  //send saltedHash to the database
+  Users.findOne({ username: `${userName}` }, (err, response) => {
+    if (err) {
+      res.status(503).send();
+    } else {
+      //TODO: Verify that the model gets returned back
+    }
+  });
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+//If user is not registered, post their info into DB for validation
+app.post('/registration', (req, res) => {
+  const { username, password } = req.body;
+  if (!username && !password) {
+    res.send('Requires both form fields to be filled out');
+  }
+
+  //TODO
+  if (!username) {
+    let missingVal = !userName ? 'password' : 'username';
+    res.status(418).send(`Error, requires a ${missingVal}`);
+  }
+
+  if (!password) {
+    res.status(418).send('Error, requires a valid password');
+  }
+
+  if (username && password) {
+    const User = new Users(user); //TODO: double check on this
+    User.setPassword(password);
+  }
+});
+
+module.exports = {
+  app,
+};
